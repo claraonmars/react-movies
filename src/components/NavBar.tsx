6e9bb11cf7c32f607a6c11867087368d7636d9ea
claraonmars <claraonmars@hotmail.com>
2020-09-24 09:06:41 +0800

    feat(navbar): add redirection to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { Link, useRouteMatch } from 'react-router-dom'
 
 const { Header } = Layout;
 
-const NavBar = () => {
+const NavBar = (): JSX.Element => {
+  const match = useRouteMatch({
+    path: "/",
+    strict: true,
+    sensitive: true
+  });
+
   return (
     <Header style={{background: 'rgba(0,0,0,0)'}}>
-      <Menu mode="horizontal">
-        <Menu.Item key="1">Main</Menu.Item>
+      <Menu mode="horizontal" selectedKeys={match?.isExact ? ["1"]: []}>
+        <Menu.Item key="1">
+          <Link to="/">Main
+          </Link></Menu.Item>
       </Menu>
     </Header>
   );
