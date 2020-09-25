fd997d71cbc3f03834aa9afcb79ecf99829e3be3
claraonmars <claraonmars@hotmail.com>
2020-09-25 09:22:19 +0800

    test(filters): add tests for filter system

--- src/__tests__/MovieList.test.tsx
+++ src/__tests__/MovieList.test.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { render, fireEvent, waitForElement,  screen } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 
 import App from "../App";
+import Main from "../pages/Main";
 import MovieList from "../components/MovieList";
 import { mockMovies } from "../utils/mockData";
+import MovieContext from "../contexts/MovieContext";
 
 beforeAll(() => {
   Object.defineProperty(window, "matchMedia", {
@@ -35,19 +37,88 @@ describe("Main movie list page", () => {
     expect(getByText("Deadpool")).toBeInTheDocument();
   });
 
-  test("should display empty message", async () => {
+  test("should display empty message", () => {
     const { getByText } = render(<MovieList movies={[]} />);
     expect(getByText("No data found")).toBeInTheDocument();
   });
 
   test("should redirect to detail page on movie selection", async () => {
-      const { getByText, container } = render(
+    const { getByText, container } = render(
       <Router history={history}>
         <App />
-      </Router>);
+      </Router>
+    );
 
-      await waitForElement(() => getByText("Main"))
-      fireEvent.click(getByText('Deadpool'))
-      expect(container.querySelectorAll('.ant-typography')[2]).toHaveTextContent('Synopsis:')
-  })
+    await waitForElement(() => getByText("Main"));
+    fireEvent.click(getByText("Deadpool"));
+    expect(container.querySelectorAll(".ant-typography")[2]).toHaveTextContent(
+      "Synopsis:"
+    );
+  });
+
+  test("should apply selected genre filters to list", async () => {
+    const { getByText, getByLabelText } = render(
+      <Router history={history}>
+        <MovieContext.Provider value={{ movies: mockMovies }}>
+          <Main />
+        </MovieContext.Provider>
+      </Router>
+    );
+    fireEvent.click(getByText("Filter By"));
+    const modal = await waitForElement(() => getByText("Animation"));
+    expect(modal).toHaveTextContent('Animation');
+
+    const checkbox = getByLabelText('Animation')
+    fireEvent.click(checkbox);
+    const applyButton = getByText('Apply')
+    fireEvent.click(applyButton)
+    expect(getByText("Barnyard")).toBeInTheDocument();
+
+    const movieCard = screen.queryByText('Deadpool')
+    expect(movieCard).toBeNull()   
+  });
+
+  test("should render no data if filters return empty list", async () => {
+    const { getByText, getByLabelText } = render(
+      <Router history={history}>
+        <MovieContext.Provider value={{ movies: mockMovies }}>
+          <Main />
+        </MovieContext.Provider>
+      </Router>
+    );
+    fireEvent.click(getByText("Filter By"));
+    const modal = await waitForElement(() => getByText("Animation"));
+    expect(modal).toHaveTextContent('Animation');
+
+    const genreCheckbox = getByLabelText('Animation')
+    fireEvent.click(genreCheckbox);
+    const yearCheckbox = getByLabelText('2016')
+    fireEvent.click(yearCheckbox);
+    const applyButton = getByText('Apply')
+    fireEvent.click(applyButton)
+
+    expect(getByText("No data found")).toBeInTheDocument();
+  });
+
+  test("should not filter movies if modal is closed without applying", async () => {
+    const { getByText, getByLabelText, getByRole } = render(
+      <Router history={history}>
+        <MovieContext.Provider value={{ movies: mockMovies }}>
+          <Main />
+        </MovieContext.Provider>
+      </Router>
+    );
+    fireEvent.click(getByText("Filter By"));
+    const modal = await waitForElement(() => getByText("Animation"));
+    expect(modal).toHaveTextContent('Animation');
+
+    const genreCheckbox = getByLabelText('Animation')
+    fireEvent.click(genreCheckbox);
+    const yearCheckbox = getByLabelText('2016')
+    fireEvent.click(yearCheckbox);
+    const cancelButton = getByRole('img')
+    fireEvent.click(cancelButton)
+
+    expect(getByText("Deadpool")).toBeInTheDocument();
+  });
 });
