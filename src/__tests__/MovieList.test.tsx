fae039759d127e42f35189ac34803c402e531e21
claraonmars <claraonmars@hotmail.com>
2020-09-28 14:03:59 +0800

    test(filters): update tests for list filters

--- src/__tests__/MovieList.test.tsx
+++ src/__tests__/MovieList.test.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { render, fireEvent, waitForElement,  screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  waitForElement,
+  screen,
+} from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 
 import App from "../App";
 import Main from "../pages/Main";
 import MovieList from "../components/MovieList";
-import { mockMovies } from "../utils/mockData";
 import MovieContext from "../contexts/MovieContext";
+import {
+  mockMovies,
+  initialState as state,
+  mockDispatch as dispatch,
+} from "./utils/mockData";
+import FiltersModal from "../components/Filters";
 
 beforeAll(() => {
   Object.defineProperty(window, "matchMedia", {
@@ -57,45 +67,85 @@ describe("Main movie list page", () => {
   });
 
   test("should apply selected genre filters to list", async () => {
-    const { getByText, getByLabelText } = render(
+    const handleClose = jest.fn();
+    const {
+      getByText,
+      getByLabelText,
+      rerender
+    } = render(
       <Router history={history}>
-        <MovieContext.Provider value={{ movies: mockMovies }}>
-          <Main />
+        <MovieContext.Provider value={{ state, dispatch }}>
+          <Main>
+            <FiltersModal visible={false} setVisible={handleClose} />
+          </Main>
         </MovieContext.Provider>
       </Router>
     );
     fireEvent.click(getByText("Filter By"));
     const modal = await waitForElement(() => getByText("Animation"));
-    expect(modal).toHaveTextContent('Animation');
+    expect(modal).toBeInTheDocument();
 
-    const checkbox = getByLabelText('Animation')
+    const checkbox = getByLabelText("Animation");
     fireEvent.click(checkbox);
-    const applyButton = getByText('Apply')
-    fireEvent.click(applyButton)
+    const applyButton = getByText("Apply");
+    fireEvent.click(applyButton);
+
+    rerender(
+      <Router history={history}>
+        <MovieContext.Provider
+          value={{
+            state: {
+              ...state,
+              filtersApplied: { genre: ["Animation"], year: [] },
+            },
+            dispatch,
+          }}
+        >
+          <Main />
+        </MovieContext.Provider>
+      </Router>
+    );
+
     expect(getByText("Barnyard")).toBeInTheDocument();
 
-    const movieCard = screen.queryByText('Deadpool')
-    expect(movieCard).toBeNull()   
+    const movieCard = screen.queryByText("Deadpool");
+    expect(movieCard).toBeNull();
   });
 
   test("should render no data if filters return empty list", async () => {
-    const { getByText, getByLabelText } = render(
+    const { getByText, getByLabelText, rerender } = render(
       <Router history={history}>
-        <MovieContext.Provider value={{ movies: mockMovies }}>
+        <MovieContext.Provider value={{ state, dispatch }}>
           <Main />
         </MovieContext.Provider>
       </Router>
     );
     fireEvent.click(getByText("Filter By"));
     const modal = await waitForElement(() => getByText("Animation"));
-    expect(modal).toHaveTextContent('Animation');
+    expect(modal).toHaveTextContent("Animation");
 
-    const genreCheckbox = getByLabelText('Animation')
+    const genreCheckbox = getByLabelText("Animation");
     fireEvent.click(genreCheckbox);
-    const yearCheckbox = getByLabelText('2016')
+    const yearCheckbox = getByLabelText("2016");
     fireEvent.click(yearCheckbox);
-    const applyButton = getByText('Apply')
-    fireEvent.click(applyButton)
+    const applyButton = getByText("Apply");
+    fireEvent.click(applyButton);
+
+    rerender(
+      <Router history={history}>
+        <MovieContext.Provider
+          value={{
+            state: {
+              ...state,
+              filtersApplied: { genre: ["Animation"], year: [2016] },
+            },
+            dispatch,
+          }}
+        >
+          <Main />
+        </MovieContext.Provider>
+      </Router>
+    );
 
     expect(getByText("No data found")).toBeInTheDocument();
   });
@@ -103,21 +153,21 @@ describe("Main movie list page", () => {
   test("should not filter movies if modal is closed without applying", async () => {
     const { getByText, getByLabelText, getByRole } = render(
       <Router history={history}>
-        <MovieContext.Provider value={{ movies: mockMovies }}>
+        <MovieContext.Provider value={{ state, dispatch }}>
           <Main />
         </MovieContext.Provider>
       </Router>
     );
     fireEvent.click(getByText("Filter By"));
     const modal = await waitForElement(() => getByText("Animation"));
-    expect(modal).toHaveTextContent('Animation');
+    expect(modal).toHaveTextContent("Animation");
 
-    const genreCheckbox = getByLabelText('Animation')
+    const genreCheckbox = getByLabelText("Animation");
     fireEvent.click(genreCheckbox);
-    const yearCheckbox = getByLabelText('2016')
+    const yearCheckbox = getByLabelText("2016");
     fireEvent.click(yearCheckbox);
-    const cancelButton = getByRole('img')
-    fireEvent.click(cancelButton)
+    const cancelButton = getByRole("img");
+    fireEvent.click(cancelButton);
 
     expect(getByText("Deadpool")).toBeInTheDocument();
   });
